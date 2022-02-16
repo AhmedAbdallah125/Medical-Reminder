fbcdb48909d9609a48da2ca24d0483fb0ad26862
AbdelrhmanSayed <abdosayed20162054@gmail.com>
2022-02-16 20:33:10 +0200

    prepare database interfaces

---
+++ app/src/main/java/com/team_three/medicalreminder/dataBase/ApplicationDataBase.java
@@ -0,0 +1,5 @@
+package com.team_three.medicalreminder.dataBase;
+
+
+public class ApplicationDataBase {
+}
---
+++ app/src/main/java/com/team_three/medicalreminder/dataBase/ConcreteLocalClass.java
@@ -0,0 +1,4 @@
+package com.team_three.medicalreminder.dataBase;
+
+public class ConcreteLocalClass {
+}
