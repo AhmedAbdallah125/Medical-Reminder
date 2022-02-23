104e099a856d4543cbad960a48cbc7bff4d1242a
Ahmed Mohamed Abdallah <benten12536.com>
2022-02-24 00:36:15 +0200

    handleBackAndChangeUi

--- app/src/main/java/com/team_three/medicalreminder/homeScreen/view/HomeActivity.java
+++ app/src/main/java/com/team_three/medicalreminder/homeScreen/view/HomeActivity.java
@@ -1,24 +1,15 @@
 package com.team_three.medicalreminder.homeScreen.view;
 
 
-import android.Manifest;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 ;
 
 import com.google.android.material.navigation.NavigationView;
-import com.team_three.medicalreminder.MainActivity;
 import com.team_three.medicalreminder.R;
 import com.team_three.medicalreminder.Registeration.view.LoginActivity;
 import com.team_three.medicalreminder.Registeration.view.RegisterFragment;
@@ -27,27 +18,16 @@ import com.team_three.medicalreminder.databinding.HomeDrawerBinding;
 import com.team_three.medicalreminder.model.Repository;
 import com.team_three.medicalreminder.network.FireBaseNetwork;
 import com.team_three.medicalreminder.network.NetworkInterface;
-import com.team_three.medicalreminder.workmanger.MyPeriodicWorkManger;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-import androidx.work.Constraints;
-import androidx.work.ExistingPeriodicWorkPolicy;
-import androidx.work.PeriodicWorkRequest;
-import androidx.work.WorkManager;
-import androidx.work.impl.model.Preference;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -59,8 +39,6 @@ public class HomeActivity extends AppCompatActivity {
     String name = "";
     String email = "";
     Repository myRepository;
-    //
-    private static final int RC_OVERLAY = 33;
 
 
     // synchronisation when network is connected
@@ -74,24 +52,6 @@ public class HomeActivity extends AppCompatActivity {
         initNavigation();
         handleDrawerMenu();
 //        setSignOut(true);
-        ///
-
-
-        //2
-        if (ContextCompat.checkSelfPermission(
-                this, Settings.ACTION_MANAGE_OVERLAY_PERMISSION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            // may be wrong
-            setWorkTimer();
-
-        } else if (shouldShowRequestPermissionRationale(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)) {
-            Toast.makeText(this, "should accept permission to start Alarm ", Toast.LENGTH_SHORT).show();
-        } else {
-            // You can directly ask for the permission.
-            // The registered ActivityResultCallback gets the result of this request.
-            requestPermissionLauncher.launch(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-        }
         //
         navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
             @Override
@@ -111,9 +71,6 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
-
-///////////////////////////////
-
 
 
     }
@@ -189,7 +146,7 @@ public class HomeActivity extends AppCompatActivity {
                     if (item.getItemId() == R.id.nav_logout) {
                         setSignOut();
                         return true;
-                    } else if (item.getItemId() == R.id.patients) {
+                    }else if(item.getItemId() == R.id.patients){
 
                         return true;
                     }
@@ -248,85 +205,13 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
+
     private void handleSignOutCondition() {
         editor = sharedPref.edit();
         editor.putString(RegisterFragment.USER_EMAIL, "null");
         editor.putString(RegisterFragment.USER_NAME, "null");
         editor.apply();
     }
-
-    // return after requesting Permission
-
-    private final ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    setWorkTimer();
-                } else {
-                    final Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                            Uri.parse("package:" + getPackageName()));
-                    try {
-                        startActivityForResult(intent, RC_OVERLAY);
-                    } catch (ActivityNotFoundException e) {
-                        Log.e("MainActivity", e.getMessage());
-                    }
-                }
-            });
-
-    private void setWorkTimer() {
-        Constraints constraints = new Constraints.Builder()
-                .setRequiresBatteryNotLow(true)
-                .build();
-
-        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyPeriodicWorkManger.class,
-                6, TimeUnit.HOURS)
-                .setConstraints(constraints)
-                .build();
-
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork("Counter", ExistingPeriodicWorkPolicy.KEEP, periodicWorkRequest);
-//        WorkManager.getInstance(this).enqueue(periodicWorkRequest);
-    }
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                                           int[] grantResults) {
-//
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        switch (requestCode) {
-//            case 33:
-//                // If request is cancelled, the result arrays are empty.
-//                if (grantResults.length > 0 &&
-//                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    // Permission is granted. Continue the action or workflow
-//                    // in your app.
-//                } else {
-//                    // Explain to the user that the feature is unavailable because
-//                    // the features requires a permission that the user has denied.
-//                    // At the same time, respect the user's decision. Don't link to
-//                    // system settings in an effort to convince the user to change
-//                    // their decision.
-//                }
-//                return;
-//        }
-//        // Other 'case' lines to check for other
-//        // permissions this app might request.
-//    }
-//}
-@Override
-protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    switch (requestCode) {
-        case RC_OVERLAY:
-            final boolean overlayEnabled = Settings.canDrawOverlays(this);
-            if (overlayEnabled) {
-                Toast.makeText(this, "xxxxxxx", Toast.LENGTH_SHORT).show();
-                setWorkTimer();
-            } else {
-//                    switchCyberBulling.setChecked(false);
-            }
-            // Do something...
-            break;
-    }
-}
-
 }
 
 
