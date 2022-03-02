5af4bf39489b5d322a0aed5977d495aef6b40fc6
MohabElziny <mohabziny@gmail.com>
2022-03-02 22:40:22 +0200

    checked pref and disable rotation

--- app/src/main/java/com/team_three/medicalreminder/model/Utility.java
+++ app/src/main/java/com/team_three/medicalreminder/model/Utility.java
@@ -2,10 +2,16 @@ package com.team_three.medicalreminder.model;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import com.team_three.medicalreminder.Registeration.view.RegisterFragment;
+
 public class Utility {
+
+    private static SharedPreferences sharedPref;
+    private static String email;
 
     public static boolean isOnline(Context context) {
         ConnectivityManager cm =
@@ -13,4 +19,11 @@ public class Utility {
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
+
+    public static String checkShared(Context context){
+        sharedPref = context.getSharedPreferences(RegisterFragment.SHAREDfILE, Context.MODE_PRIVATE);
+        email = sharedPref.getString(RegisterFragment.USER_EMAIL, "null");
+        return email;
+    }
+
 }
